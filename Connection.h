dda1a22b3aaf54f0d8735a6285f63fb68cc23361
Breno Guimaraes <brenorg@gmail.com>
2020-12-19 08:26:28 -0500

    Extract code

--- Connection.h
+++ Connection.h
@@ -2,6 +2,7 @@
 
 #include <cassert>
 #include <optional>
+#include <cstring>
 
 #include <netinet/in.h>
 #include <sys/select.h>
@@ -25,3 +26,21 @@ struct Connection
     }
     int fd {-1};
 };
+
+inline std::string read(const Connection& conn)
+{
+    char buffer[10*256];
+    std::memset(buffer, 0, 10*256);
+    auto n = read(conn.fd, buffer, 10*256-1);
+    return std::string(buffer, n);
+}
+
+inline void write(const Connection& conn, std::string_view buf)
+{
+    while (!buf.empty())
+    {
+        auto n = write(conn.fd, buf.data(), buf.size());
+        buf = buf.substr(n);
+    }
+}
+
--- FlameServer.h
+++ FlameServer.h
@@ -16,11 +16,7 @@ struct FlameServer
     void operator()(ThreadPool&)
     {
         std::cout << "Got a connection" << std::endl;
-        char buffer[10*256];
-        std::memset(buffer, 0, 10*256);
-        auto n = read(conn.fd, buffer, 10*256-1);
-
-        auto req = parse_html_request({buffer, static_cast<unsigned long>(n)});
+        auto req = parse_html_request(read(conn));
 
         std::string body = "<html><body>\n";
 
@@ -28,7 +24,7 @@ struct FlameServer
 
         if (fs::is_directory(fspath))
         {
-            for (const auto & entry : fs::directory_iterator(fspath))
+            for (const auto & entry : fs::directory_iterator(fspath, fs::directory_options::skip_permission_denied))
             {
                 auto entrypath = entry.path().string();
                 body += "<a href=\"" + entrypath + "\">" + entrypath + "</a><br>\n";
@@ -41,10 +37,9 @@ struct FlameServer
 
         body += "\n</body></html>";
 
-        Response r = make_html_response(std::move(body));
-        auto buf = r.to_bytes();;
-        n = write(conn.fd, buf.data(), buf.size());
-        std::cout << "Wrote back " << n << " bytes" << std::endl;
+        auto buf = make_html_response(std::move(body)).to_bytes();
+        write(conn, buf);
+        std::cout << "Wrote back " << buf.size() << " bytes" << std::endl;
 
     }
 
