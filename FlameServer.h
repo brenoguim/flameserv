b7a3911a016e67bcf0809038ab7316513127b428
Breno Guimaraes <brenorg@gmail.com>
2020-12-19 08:08:45 -0500

    More

--- FlameServer.h
+++ FlameServer.h
@@ -3,8 +3,11 @@
 #include "Connection.h"
 #include "Http.h"
 
-#include <iostream>
 #include <cstring>
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
 
 class ThreadPool;
 
@@ -17,7 +20,28 @@ struct FlameServer
         std::memset(buffer, 0, 10*256);
         auto n = read(conn.fd, buffer, 10*256-1);
 
-        Response r = make_html_response(__PRETTY_FUNCTION__);
+        auto req = parse_html_request({buffer, static_cast<unsigned long>(n)});
+
+        std::string body = "<html><body>\n";
+
+        fs::path fspath {req.path};
+
+        if (fs::is_directory(fspath))
+        {
+            for (const auto & entry : fs::directory_iterator(fspath))
+            {
+                auto entrypath = entry.path().string();
+                body += "<a href=\"" + entrypath + "\">" + entrypath + "</a><br>\n";
+            }
+        }
+        else
+        {
+            body += "You would see this file now: " + fspath.string();
+        }
+
+        body += "\n</body></html>";
+
+        Response r = make_html_response(std::move(body));
         auto buf = r.to_bytes();;
         n = write(conn.fd, buf.data(), buf.size());
         std::cout << "Wrote back " << n << " bytes" << std::endl;
--- Http.h
+++ Http.h
@@ -7,6 +7,7 @@
 #include <optional>
 #include <stdexcept>
 #include <vector>
+#include <sstream>
 
 struct HttpMessage
 {
@@ -16,6 +17,8 @@ struct HttpMessage
 
 struct Request : public HttpMessage
 {
+    std::string operation;
+    std::string path;
 };
 
 struct Response : public HttpMessage
@@ -36,6 +39,8 @@ std::string Response::to_bytes() const
         }
         r += '\n';
     }
+    r += "Content-Length: " + std::to_string(m_body.size()) + '\n';
+
     r += '\n';
     r += m_body;
     return r;
@@ -48,5 +53,28 @@ inline Response make_html_response(std::string body)
     r.m_attrs["Content-Type"].push_back("charset=UTF-8");
     r.m_attrs["Connection"].push_back("close");
     r.m_body = std::move(body);
+    return r;
+}
+
+
+// GET / HTTP/1.1
+// Host: 192.168.15.14:8081
+// Connection: keep-alive
+// Cache-Control: max-age=0
+// DNT: 1
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+// Accept-Encoding: gzip, deflate
+// Accept-Language: en-US,en;q=0.9,de;q=0.8,pt;q=0.7,es;q=0.6
+
+inline Request parse_html_request(std::string_view str)
+{
+    std::istringstream ss{std::string(str)};
+
+    Request r;
+    ss >> r.operation;
+    ss >> r.path;
+
     return r;
 }
